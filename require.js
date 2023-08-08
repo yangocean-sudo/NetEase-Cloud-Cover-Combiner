38a37ecb042c8bd341b4ecc207ee6af6d86d0b2c
yangocean-sudo <1667600857@qq.com>
2023-08-08 14:39:43 +0800

    Can visit playlist and return a combined image

--- require.js
+++ require.js
@@ -5,11 +5,9 @@ const async = require("async");
 const request = require("request");
 const path = require("path");
 const sharp = require('sharp');
-const { download } = require('express/lib/response');
-
+const retry = require('retry');
+// const gridSize = 10
 const imageSize = 100; // Size of each individual image (adjust as needed)
-const gridSize = 15; // Number of images in each row and column
-const outputSize = imageSize * gridSize;
 const imagesPath = path.join(__dirname, 'images');
 const outputImagePath = path.join(__dirname, 'combined.jpg');
 
@@ -37,15 +35,21 @@ const combineImages = async () => {
         const imageFiles = fs.readdirSync(imagesPath)
         const resizedImages = await Promise.all(
             imageFiles.map(async (filename) => {
-                const imagePath = path.join(imagesPath, filename);
-                const image = await sharp(imagePath).resize(imageSize, imageSize).toBuffer()
+                const imagePath = path.join(imagesPath, filename)
+                const image = await sharp(imagePath).toBuffer()
+
+                // const image = await sharp(imagePath).resize(imageSize, imageSize).toBuffer()
                 // const resizedImagePath = path.join(resizedImagesPath, filename);
                 // fs.writeFileSync(resizedImagePath, image);
                 return image
             })
         )
+        const gridSize = Math.floor(Math.sqrt(resizedImages.length)) // Number of images in each row and column
+
         // Calculate the dimensions of the combined image
         const combinedSize = gridSize * imageSize
+
+
         // Create a new image with white background
         const combinedImage = sharp({
             create: {
@@ -73,14 +77,14 @@ const combineImages = async () => {
         combinedImage.composite(compositeOperations)
         // Save the combined image
         await combinedImage.toFile(outputImagePath, { quality: 100, force: true })
-        console.log('Images combined successfully.')
     } catch (error) {
         console.error('Error combining images:', error)
     }
 }
 
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-http.get('http://localhost:3000/playlist/track/all?id=8612509110', function (res) {
+http.get('http://localhost:3000/playlist/track/all?id=8480608218', function (res) {
     let json = ''
     res.on('data', function (chunk) {
         // console.log(chunk + '')
@@ -95,23 +99,46 @@ http.get('http://localhost:3000/playlist/track/all?id=8612509110', function (res
 
             // print total number of songs
             console.log("Total number of songs:", songs.length)
+
+            // retry options
+            const retryOptions = {
+                retries: 20, // Number of retry attempts
+                minTimeout: 10000, // Minimum delay between retries in milliseconds
+            };
+
             for (let i = 0; i < songs.length; i++) {
+                // if (downloadList.length >= gridSize * gridSize) {
+                //     console.log('Image downloading complete.')
+                //     break;
+                // }
                 const al = songs[i]
                 if (!downloadList.includes(al.al.picUrl)) {
-                    const filename = path.basename(al.al.picUrl);
-                    const dest = path.join(__dirname, 'images', filename);
+                    const filename = path.basename(al.al.picUrl)
+                    const dest = path.join(__dirname, 'images', filename)
+                    const resizedpicUrl = al.al.picUrl + '?param=100y100'
                     downloadList.push(al.al.picUrl)
-                    downloadImage(al.al.picUrl, dest)
-                    // downloadImage(al.al.picUrl, dest)
-                    console.log('Downloading image number: ', i + 1)
+                    // Retry downloadImage function with retry options
+                    const operation = retry.operation(retryOptions);
+                    operation.attempt(async (currentAttempt) => {
+                        try {
+                            downloadPromises.push(downloadImage(resizedpicUrl, dest))
+                            // await downloadImage(resizedpicUrl, dest);
+                            console.log('Downloading image number: ', downloadList.length);
+                        } catch (err) {
+                            if (operation.retry(err)) {
+                                return;
+                            }
+                            console.error('Error downloading image: ', err);
+                        }
+                    });
                 }
             }
 
-            // await Promise.all(downloadPromises)
-            // console.log('All images downloaded successfully.')
+            await Promise.all(downloadPromises)
+            console.log('All images downloaded successfully.')
 
-            // await combineImages()
-            // console.log('Images combined successfully.')
+            await combineImages()
+            console.log('Images combined successfully.')
         } catch (error) {
             console.error('Error parsing JSON: ', error)
         }
