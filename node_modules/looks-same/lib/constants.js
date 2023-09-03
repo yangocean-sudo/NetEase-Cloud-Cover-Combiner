f4059b60b81b01a017fb425ed3f132f87c0686f4
yangocean-sudo <1667600857@qq.com>
2023-09-03 12:53:14 +0800

    Add new feature in html, add package_json, delete module package

--- node_modules/looks-same/lib/constants.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    JND: 2.3, // Just noticeable difference if ciede2000 >= JND then colors difference is noticeable by human eye
-    REQUIRED_IMAGE_FIELDS: ['source', 'boundingBox'],
-    REQUIRED_BOUNDING_BOX_FIELDS: ['left', 'top', 'right', 'bottom'],
-    CLUSTERS_SIZE: 10,
-    DIFF_IMAGE_CHANNELS: 3
-};
